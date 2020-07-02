0f9e0b647a1dc6c5f83c38ac2bc89e8ceee66c3a
Samul <samulshrestha101@gmail.com>
2020-07-02 15:15:53 +0545

    Updates

---
+++ precache-manifest.835abdb9dffd61ecc6e6f12750450284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e920ebf4b0e7138877f4d135c8aaca",
+    "url": "/ronatracker/index.html"
+  },
+  {
+    "revision": "b7fc47ddf2fce28bb746",
+    "url": "/ronatracker/static/css/main.61db8e46.chunk.css"
+  },
+  {
+    "revision": "5780b4e870d7ef47ca43",
+    "url": "/ronatracker/static/js/2.ec2055fe.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/ronatracker/static/js/2.ec2055fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7fc47ddf2fce28bb746",
+    "url": "/ronatracker/static/js/main.e0752ec6.chunk.js"
+  },
+  {
+    "revision": "68b265daf5cc115f7f6b",
+    "url": "/ronatracker/static/js/runtime-main.a172c4ca.js"
+  }
+]);
